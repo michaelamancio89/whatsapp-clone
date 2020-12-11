5b68e3cb3d4e545165a3dda50459c1e40dd08cfc
Michael Amâncio <michael89amancio@gmail.com>
2020-12-11 16:22:55 -0300

    take a picture

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,4 +1,6 @@
-class WhatsAppController{
+import {Format} from './../util/Format';
+import {CameraController} from './CameraController';
+export class WhatsAppController{
 
     constructor(){
         console.log('WhatsAppController OK!');
@@ -214,11 +216,33 @@ class WhatsAppController{
 
             this.closeAllMainPanel();
             this.el.panelMessagesContainer.show();
+            this._camera.stop();
         });
 
         this.el.btnTakePicture.on('click', e => {
 
-            console.log('take picture');
+            let dataUrl = this._camera.takePicture();
+
+            this.el.pictureCamera.src = dataUrl;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
+        });
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+        });
+
+        this.el.btnSendPicture.on('click', e => {
+
+            console.log(this.el.pictureCamera.src);
         });
 
         this.el.btnAttachDocument.on('click', e => {
