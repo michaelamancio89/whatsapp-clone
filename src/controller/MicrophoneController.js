3ac004689beccd6f9c63207ba64b03c8be5fd3d0
Michael Amâncio <michael89amancio@gmail.com>
2020-12-14 19:19:44 -0300

    start and stop recorder

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -1,12 +1,13 @@
 import { ClassEvent } from "../util/ClassEvent";
-
-import {ClassEvent} from '../util/ClassEvent';
-
 export class MicrophoneController extends ClassEvent {
 
     constructor(){
 
         super();
+
+        this._mimeType = 'audio/webm';
+
+        this._available = false;
 
         navigator.mediaDevices.getUserMedia({
 
@@ -14,12 +15,9 @@ export class MicrophoneController extends ClassEvent {
             
         }).then(stream => {
 
+            this._available = true;
             this._stream = stream;
-
-            let audio = new Audio();
-            audio.src = URL.createObjectURL(stream);
-            audio.play();
-            this.trigger('play',audio);
+            this.trigger('ready', audio);
 
         }).catch(err =>{
 
@@ -28,12 +26,84 @@ export class MicrophoneController extends ClassEvent {
 
     }
 
+    isAvailable(){
+
+        return this._available;
+    }
+
     stop(){
 
         this._stream.getTracks().forEach(track => {
             track.stop();
 
         });
+    }
+
+    startRecorder(){
+
+        if (this.isAvailable()) {
+
+            this._mediaRecorder = new MediaRecorder(this._stream, {
+
+                mimeType: this._mimeType
+            });
+
+            this._recordedChunks = [];
+
+            this._mediaRecorder.addEventListener('dataavailable', e => {
+
+
+                if (e.data.size > 0) this._recordedChunks.push(e.data);
+
+            });
+
+            this._mediaRecorder.addEventListener('stop', e => {
+
+                let blob = new Blob(this._recordedChunks, {
+
+                    type: this._mimeType
+                });
+
+                let filename = `rec${Date.now()}.webm`;
+
+                let file = new File([blob], filename, {
+                    type:this._mimeType,
+                    lastModified: Date.now()
+                });
+
+                console.log('file',file);
+
+                let reader = new FileReader();
+
+                reader.onload = e => {
+
+                    console.log('reader file',file);
+
+                    let audio = new Audio(reader.result);
+
+                    audio.play();
+
+                }
+
+                reader.readAsDataURL(file);
+
+            });
+
+            this._mediaRecorder.start();
+
+
+        }
+
+    }
+
+    stopRecorder(){
+        
+        if (this.isAvailable()) {
+
+            this._mediaRecorder.stop();
+            this.stop();
+            
+        }
     }
 
 
