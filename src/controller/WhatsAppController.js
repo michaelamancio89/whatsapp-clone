bdc6b97b1fe3d2a49836bc4ee2a82844d7d57ed5
Michael Amâncio <michael89amancio@gmail.com>
2021-01-03 20:03:15 -0300

    send contacts

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -7,6 +7,7 @@ import { User } from "../model/User";
 import { Chat } from "../model/Chat";
 import { Message } from "./../model/Message";
 import { Base64 } from "./../util/Base64";
+import { ContactsController } from "./../controller/ContactsController";
 export class WhatsAppController{
 
     constructor(){
@@ -693,13 +694,24 @@ export class WhatsAppController{
 
         this.el.btnAttachContact.on('click', e => {
 
-            this.el.modalContacts.show();
+            this._contactsController = new ContactsController(this.el.modalContacts, this._user);
+
+            this._contactsController.on('select', contact => {
+
+                Message.sendContact(this._contactActive.chatId,
+                                    this._user.email,
+                                    contact
+                                    );
+
+            });
+
+            this._contactsController.open();
         });
 
         this.el.btnCloseModalContacts.on('click', e => {
 
-            this.closeAllMainPanel();
-            this.el.panelMessagesContainer.show();
+
+            this._contactsController.close();
         });
 
         this.el.btnSendMicrophone.on('click', e => {
--- src/model/Message.js
+++ src/model/Message.js
@@ -47,6 +47,7 @@ export class Message extends Model{
 
         let div = document.createElement('div');
 
+        div.id = `_${this.id}`
         div.className = 'message';
 
         switch (this.type) {
@@ -74,7 +75,7 @@ export class Message extends Model{
                                 </div>
                             </div>
                             <div class="_1lC8v">
-                                <div dir="ltr" class="_3gkvk selectable-text invisible-space copyable-text">Nome do Contato Anexado</div>
+                                <div dir="ltr" class="_3gkvk selectable-text invisible-space copyable-text">${this.content.name}</div>
                             </div>
                             <div class="_3a5-b">
                                 <div class="_1DZAH" role="button">
@@ -89,6 +90,20 @@ export class Message extends Model{
 
                 </div>
                 `;
+
+                if (this.content.photo) {
+
+                    let img = div.querySelector('.photo-contact-sended');
+                    img.src = this.content.photo;
+                    img.show();
+
+                }
+
+                div.querySelector('.btn-message-send').on('click', e => {
+
+                    console.log('Enviar mensagem');
+                });
+
                 break;
 
             case 'document':
@@ -282,7 +297,7 @@ export class Message extends Model{
 
             default:
                 div.innerHTML =`
-                <div class="font-style _3DFk6  tail" id="_${this.id}">
+                <div class="font-style _3DFk6  tail">
                     <span class="tail-container"></span>
                     <span class="tail-container highlight"></span>
                     <div class="Tkt2p">
@@ -339,6 +354,11 @@ export class Message extends Model{
         });
 
 
+    }
+
+    static sendContact(chatId, from, contact){
+
+        return Message.send(chatId, from, 'contact', contact);
     }
 
     static sendDocument(chatId, from, file, filePreview, info){
